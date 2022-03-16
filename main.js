0dc8729db113b59f9899f00f11bf3c90038b2a7b
Stratton Brooks <69229545+szbrooks2017@users.noreply.github.com>
2022-03-16 13:54:17 -0500

    adding filler html,css, text

--- main.js
+++ main.js
@@ -1,6 +1,105 @@
-import './style.css'
+import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
+// scene
+const scene = new THREE.Scene();
+
+// camera
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+// renderer
+const renderer = new THREE.WebGLRenderer({
+  // select the DOM element to use
+  canvas: document.querySelector('#bg'),
+})
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+camera.position.setX(-3);
+renderer.render(scene, camera);
+
+// objects
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshStandardMaterial({color: 0xFF6347});
+const torus = new THREE.Mesh(geometry, material);
+scene.add(torus);
+
+// lights
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+// Mouse Controls
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// Space background
+/// Star method to randomly generate a star
+function addStar(){
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({color:0xffffff});
+  const star = new THREE.Mesh(geometry, material);
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+Array(200).fill().forEach(addStar);
+
+// Import texture for space
+const spaceTexture = new THREE.TextureLoader().load('/Assets/Images/space.jpg');
+scene.background = spaceTexture;
+
+// Avatar
+const stratTexture = new THREE.TextureLoader().load('/Assets/Images/2020_profile.png');
+const strat = new THREE.Mesh(
+  new THREE.BoxGeometry(3, 3, 3),
+  new THREE.MeshBasicMaterial({map: stratTexture})
+)
+scene.add(strat);
+
+// Moon
+const moonTexture = new THREE.TextureLoader().load('/Assets/Images/moon.jpg');
+const normalTexture = new THREE.TextureLoader().load('/Assets/Images/normal.jpg');
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+      map: moonTexture,
+      normalMap: normalTexture,
+    })
+)
+scene.add(moon);
+moon.position.z = 30;
+moon.position.setX(-8);
+
+strat.position.z = -5;
+strat.position.x = 2;
+
+// Camera Controls
+function moveCamera(){
+  const t = document.body.getBoundingClientRect().top;
+  moon.rotation.x += 0.05;
+  moon.rotation.y += 0.075;
+  moon.rotation.z += 0.05;
+
+  strat.rotation.y += 0.01;
+  strat.rotation.z += 0.01;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.position.y = t * -0.0002;
+}
+document.body.onscroll = moveCamera
+moveCamera();
+
+// Animation
+function animate(){
+  requestAnimationFrame(animate);
+  torus.rotation.x += 0.001;
+  torus.rotation.y += 0.0005;
+  torus.rotation.z += 0.001;
+  // controls.update();
+  renderer.render(scene, camera);
+}
+
+animate();
